d0a5aa5634fe2827c132c1021393582ae8e9b0be
Zzhiwei <zhiwei3141@gmail.com>
2022-11-16 19:06:45 +0800

    v1.2

--- proxy.py
+++ proxy.py
@@ -1,33 +1,25 @@
-import os,sys,time, socket, select
+import sys,time, socket
 import _thread as thread
-
 
 # configs
 SERVER_NAME = ''
 BACKLOG = 40
 MAX_RECV_BYTES = 256000
+PORT = IS_FLAG = ATK_FLAG = None
 
 # constants
 CLRF = b'\r\n'
 REFERER_LOCATOR = b'Referer: '
+HOST_LOCATOR = b'Host: '
 DOC_IDENTIFIER = b'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp'
 IMAGE_IDENTIFIER = b'Accept: image/avif,image/webp'
 SUBSTITUTION = b'GET http://ocna0.d2.comp.nus.edu.sg:50000/change.jpg HTTP/1.0\r\nHost: ocna0.d2.comp.nus.edu.sg:50000\r\n'
-
 STARTED = 'started'
 ENDED = 'ended'
 TOTAL = 'tolal'
-
 ATTACK_HTML = b'<html><head><title>You are being attacked</title></head><body><center><h1>You are being attacked</h1></center></body></html>'
-PORT = IS_FLAG = ATK_FLAG = None
 
-
-# connections = set([])
-
-class ProxyException(Exception):
-    pass
-
-# value: first number is opened connection, second is closed connection
+# store
 telemetry_store = dict({}) 
 
 def main():
@@ -44,64 +36,55 @@ def main():
             print('ERROR: invalid argument types, please enter numbers')
             sys.exit(1)
         
-        
     try:
         helloSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         helloSocket.bind((SERVER_NAME, PORT))
         helloSocket.listen(BACKLOG)
     except socket.error as e:
-        print('CRITICAL ERROR: fail to open hello socket')
         print(e)
+        print('ERROR: fail to open hello socket, port could be still in use')
         if helloSocket:
             helloSocket.close()
         sys.exit(1)        
 
-    # helloSocket.settimeout(7) -> can't do this because if no request, program is blocked at accept(), this is normal behavior
     while True:       
         try:
             connection, client_address = helloSocket.accept()
-            # connections.add(connection)
-            # print('number of connections:', len(connections))
+            # print('added new connection')
             thread.start_new_thread(proxy_thread, (connection, client_address))
-            
         except KeyboardInterrupt:
-            helloSocket.shutdown(socket.SHUT_RDWR)
+            helloSocket.close()
 
-# j = 0
-
+# i = 0
 def proxy_thread(client_conn: socket.socket, client_address):
-    # global j
-    # thread_no = j
-    # j+=1
+    # global i
+    # tn = i
+    # i+=1
+    timeout_count = 0
     while True:
-        
         try:
-            ## using while true will lead to diff websites using same TCP connection
-            ## (firefox uses same network client process even for different websites?)
+            client_conn.settimeout(5)
             request = client_conn.recv(MAX_RECV_BYTES)
-
+            client_conn.settimeout(None)
+            
             if not len(request): 
                 client_conn.close()
                 return
-
             if ATK_FLAG:
-                print('sending attack')
                 send_attack_message(client_conn)
                 client_conn.close()
                 return 
             
             port, hostname, http_method, url = get_fields(request)
-            # print('fields:', port, hostname, http_method, url)
-            # print(f'inside thread {thread_no}: receiving for url - {url}')
+            
             
             if is_html_request(request):
                 referer = remove_end_slash(url).decode()
             else:
                 referer = remove_end_slash(get_referer(request)).decode()
             
-            
             ## log
-            # print(http_method.decode(),'\t',f'hostname={hostname.decode()}','\t',f'url={url.decode()}', '\t', f'referer={referer}')
+            # print(http_method.decode(),'\t',f'hostname={hostname.decode()}', '\t', f'port={port}','\t',f'url={url.decode()}', '\t', f'referer={referer}')
 
             if not telemetry_store.get(referer):
                 telemetry_store[referer] = {STARTED: 1, ENDED: 0, TOTAL: 0}
@@ -140,47 +123,45 @@ def proxy_thread(client_conn: socket.socket, client_address):
                             s.settimeout(None)
                         except socket.timeout:
                             break
-                        
                     
-                    # print(f'payload_len:', payload_len)
                     telemetry_store[referer][TOTAL] += payload_len
                     telemetry_store[referer][ENDED] += 1
-                    # print(telemetry_store)
                     if telemetry_store[referer][STARTED] == telemetry_store[referer][ENDED]:
                         # time out wait to check no more requests from website
                         thread.start_new_thread(telemetry_thread, (referer,))
                         
                 except socket.error as e:
-                    print(e)
                     send_error_response(str(e), client_conn)
-        
+                    client_conn.close()
+                    sys.exit(1)
+                    
+        except socket.timeout:
+            timeout_count += 1
+            if timeout_count > 3:
+                # print('closing conn due to no usage:', tn)
+                client_conn.close()
+                break
         except ProxyException as e:
             send_error_response(str(e), client_conn)
-        except ConnectionResetError as e:
-            # print('peer reset')
+            client_conn.close()
+            sys.exit(1)
+        except (ConnectionResetError, OSError):
             return
-
-
 
 def send_attack_message(conn: socket.socket):
     conn.send(b'HTTP/1.0 200 OK' + CLRF)
     conn.send(b'Content-Type: text/html' + CLRF + CLRF)
     conn.send(ATTACK_HTML) 
-    print('finished sending attk')
-
 
 def modify_req(request: bytes):
     start_index = request.find(CLRF, request.find(CLRF) + 4) + 4
     return SUBSTITUTION + request[start_index:]
     
-    
-
 def telemetry_thread(referer: str):
     time.sleep(1)
     if telemetry_store[referer][STARTED] == telemetry_store[referer][ENDED]:
         print(f'{referer}, ', telemetry_store[referer][TOTAL])
         del telemetry_store[referer]
-            
 
 def is_html_request(request: bytes):
     return request.find(DOC_IDENTIFIER) != -1 and request.find(DOC_IDENTIFIER) < request.find(CLRF + CLRF)
@@ -195,7 +176,6 @@ def get_referer(request: bytes):
     cut_request = request[referer_pos:]
     end_pos = cut_request.find(CLRF)
     referer = cut_request[9:end_pos]
-    # need error handling for if request is invalid
     return referer
 
 def remove_end_slash(s: bytes):
@@ -235,25 +215,19 @@ def get_fields(request: bytes):
         raise ProxyException('invalid url provided')
 
     return port, hostname, http_method, url
- 
-
 
 def send_error_response(e: str, client_conn: socket.socket):
-    client_conn.sendall(b"HTTP/1.0 400 Bad Request" + CLRF + CLRF 
-                                    + b"###############################################\r\n"
-                                    + b"Invalid Request. Please Check Your Request.\r\n"
-                                    + e.encode()
-                                    + b'\r\n'
-                                    + b"###############################################\r\n")
+    # use HTTP 1.0 (works for hosts using both 1.0 and 1.1 due to backward compatibility)
+    try:
+        client_conn.sendall(b'HTTP/1.0 400 Bad Request' + CLRF)
+        client_conn.sendall(b'Content-Type: text/html' + CLRF*2)
+        client_conn.sendall(b'<h1>Invalid Request: %s</h1>' % e.encode())
+        client_conn.sendall(b'<h2>Check your request and try again!</h2>')
+    except BrokenPipeError:
+        return
+
+class ProxyException(Exception):
+    pass
 
 if __name__ == '__main__':
    main()
-
-
-
-
-
-
-   
-
-
