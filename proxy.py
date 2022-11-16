3a048de67a8d231bc954162bc2f9da18e121ca34
Zzhiwei <zhiwei3141@gmail.com>
2022-11-16 11:31:18 +0800

    add exception

--- proxy.py
+++ proxy.py
@@ -10,6 +10,9 @@ CLRF = b'\r\n'
 REFERER_LOCATOR = b'Referer: '
 DOC_IDENTIFIER = b'Accept: text/html'
 
+class ProxyException(Exception):
+    pass
+
 
 # value: first number is opened connection, second is closed connection
 referer_db = dict({ 'dummy': [0, 0] }) 
@@ -40,22 +43,24 @@ def main():
             i+=1
             thread.start_new_thread(proxy_thread, (connection, client_address))
         except KeyboardInterrupt:
-            helloSocket.shutdown(socket.SHUT_WR)
+            helloSocket.shutdown(socket.SHUT_RDWR)
 
 
 def proxy_thread(client_conn: socket.socket, client_address):
-    referers = {}
-    # keeping this will lead to diff websites using same TCP connection
-    # (firefox uses same network client process even for different websites?)
-    while True: 
+    try:
+        referers = {}
+        ## using while true will lead to diff websites using same TCP connection
+        ## (firefox uses same network client process even for different websites?)
+        # while True: 
         request = client_conn.recv(MAX_RECV_BYTES) # get client request
         if not len(request): 
             client_conn.close()
             return
         
         port, hostname, http_method, url = get_fields(request)
-        # get origin
-        if request.find(DOC_IDENTIFIER) != -1 and request.find(DOC_IDENTIFIER) < request.find(CLRF + CLRF):
+        # print('fields:', port, hostname, http_method, url)
+        
+        if is_html_request(request):
             referer = remove_end_slash(url)
         else:
             referer = remove_end_slash(get_referer(request))
@@ -64,9 +69,9 @@ def proxy_thread(client_conn: socket.socket, client_address):
             referers[referer] = 0 # init 0 bytes
             
         
-        print(http_method.decode(),'\t',url.decode(), '\t', f'referer={referer.decode()}')
+        print(http_method.decode(),'\t',f'hostname={hostname.decode()}','\t',f'url={url.decode()}', '\t', f'referer={referer.decode()}')
             
-        # make request on behalf of client
+        ## make request on behalf of client
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
                 s.connect((hostname, port))
@@ -81,26 +86,35 @@ def proxy_thread(client_conn: socket.socket, client_address):
                         if not in_body:
                             header += s.recv(8)
                             header_end_pos = header.find(CLRF+CLRF)
-                            payload_len += len(header[header_end_pos+8:])
-                            client_conn.sendall(header)
-                            in_body = False
+                            if header_end_pos != -1:
+                                payload_len += len(header[header_end_pos+4:])
+                                client_conn.sendall(header)
+                                in_body = True
                         else:
                             content = s.recv(4096)
                             size = len(content)
                             if size:
                                 payload_len += size
                                 client_conn.sendall(content)
-                        s.settimeout(None)
+                            s.settimeout(None)
                     except socket.timeout:
                         break
-                
+                    
+                print(payload_len)
+            
             except socket.error as e:
                 print(e)
-                client_conn.sendall(b"HTTP/1.0 400 Bad Request" + CLRF)
+                send_error_response(str(e), client_conn)
+    
+    except ProxyException as e:
+        send_error_response(str(e), client_conn)
+    finally:
+        client_conn.close()
+    
             
-    # client_conn.close()
-    # sys.exit(0)
-            
+
+def is_html_request(request: bytes):
+    return request.find(DOC_IDENTIFIER) != -1 and request.find(DOC_IDENTIFIER) < request.find(CLRF + CLRF)
 
 def get_referer(request: bytes):
     referer_pos = request.find(REFERER_LOCATOR)
@@ -119,39 +133,55 @@ def remove_end_slash(s: bytes):
     return s[:-1] if s[-1] == 47 else s
 
 def get_fields(request: bytes):
-    first_line = request.split(CLRF)[0]
-    first_line_arr = first_line.split(b' ')
-    http_method = first_line_arr[0]
-    url =  first_line_arr[1] # example: http://localhost:3000/
-    
-    url_start = url.find(b'://')
-    if (url_start == -1):
-        truncated_url = url 
-    else:
-        truncated_url = url[url_start+3:] # example: www.google.com:3000/
-    
-    end_pos = truncated_url.find(b'/')
-    port_colon_pos = truncated_url.find(b':') 
-    
-    if end_pos == -1:
-        end_pos = len(truncated_url)
-    
-    if (port_colon_pos == -1):
-        port = 80
-        hostname = truncated_url[:end_pos]
-    elif port_colon_pos > end_pos:
-        print('ERROR: invalid url')
-        sys.exit(1)
-    else:
-        port = int(truncated_url[port_colon_pos+1:end_pos])
-        hostname = truncated_url[:port_colon_pos]
+    try:
+        first_line = request.split(CLRF)[0]
+        first_line_arr = first_line.split(b' ')
+        http_method = first_line_arr[0]
+        url =  first_line_arr[1] # example: http://localhost:3000/
+        
+        url_start = url.find(b'://')
+        if (url_start == -1):
+            truncated_url = url 
+        else:
+            truncated_url = url[url_start+3:] # example: www.google.com:3000/
+        
+        end_pos = truncated_url.find(b'/')
+        port_colon_pos = truncated_url.find(b':') 
+        
+        if end_pos == -1:
+            end_pos = len(truncated_url)
+        
+        if (port_colon_pos == -1):
+            port = 80
+            hostname = truncated_url[:end_pos]
+        elif port_colon_pos > end_pos:
+            raise ProxyException('')
+        else:
+            port = int(truncated_url[port_colon_pos+1:end_pos])
+            hostname = truncated_url[:port_colon_pos]
+    except Exception:
+        raise ProxyException('invalid url provided')
 
     return port, hostname, http_method, url
-        
+ 
+
+
+def send_error_response(e: str, client_conn: socket.socket):
+    client_conn.sendall(b"HTTP/1.0 400 Bad Request" + CLRF + CLRF 
+                                    + b"###############################################\r\n"
+                                    + b"Invalid Request. Please Check Your Request.\r\n"
+                                    + e.encode()
+                                    + b'\r\n'
+                                    + b"###############################################\r\n")
 
 if __name__ == '__main__':
    main()
-   
+
+
+
+
+
+
    
 
 
